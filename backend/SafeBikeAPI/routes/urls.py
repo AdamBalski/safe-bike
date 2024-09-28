611db6184b5db90003ca95394825ea46d3632be4
Kacper Wojtowicz <31704690+kapiw04@users.noreply.github.com>
2024-09-28 22:03:16 +0200

    added posting routs, getting danger-categories (#13)

--- backend/SafeBikeAPI/routes/urls.py
+++ backend/SafeBikeAPI/routes/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import ListRoutesView, RateView
+from .views import (
+    ListRoutesView,
+    RateView,
+    ListDangerCategoriesView,
+    CreateRouteView,
+)
 
 urlpatterns = [
     path("routes/", ListRoutesView.as_view(), name="routes"),
     path("route/<int:route_id>/score/", RateView.as_view(), name="score"),
+    path(
+        "danger-categories/",
+        ListDangerCategoriesView.as_view(),
+        name="danger-categories",
+    ),
+    path("route/", CreateRouteView.as_view(), name="create-route"),
 ]
--- backend/SafeBikeAPI/routes/views.py
+++ backend/SafeBikeAPI/routes/views.py
@@ -20,12 +20,10 @@ class ListRoutesView(views.APIView):
 
 
 class RateView(views.APIView):
-    # POST /route/$route-id/rate
-    # request body:
-    #  {“route_id”: “$route_id”, segment_ratings:
-    # {“segment_idx”: number, “danger_category_idx”: number, “value”: number}[]}
     def post(self, request, route_id):
         user_id = request.user.id
+
+        # TODO remove this line
         if not user_id:
             user_id = 1
         user = User.objects.get(id=user_id)
@@ -47,3 +45,28 @@ class RateView(views.APIView):
             score.save()
 
         return Response({"message": "Rating saved successfully"})
+
+
+class ListDangerCategoriesView(views.APIView):
+    def get(self, request):
+        danger_categories = DangerCategory.objects.all()
+        serializer = DangerCategorySerializer(danger_categories, many=True)
+        return Response(serializer.data)
+
+
+class CreateRouteView(views.APIView):
+    def post(self, request):
+        user_id = request.user.id
+        # TODO remove this line
+        if not user_id:
+            user_id = 1
+        user = User.objects.get(id=user_id)
+        segments: list = request.data["segments"]
+        route = Route.objects.create(owner_id=user, name=request.data["name"])
+        for segment in segments:
+            segment = Segment(**segment)
+            segment.save()
+            route.segments.add(segment)
+
+        route.save()
+        return Response({"message": "Route created successfully"})
