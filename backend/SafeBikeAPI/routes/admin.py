5210c068a761d399359d8dd4a59506a944ccf71f
kapiw04 <wojtowicz.kacper@gmail.com>
2024-09-28 21:10:54 +0200

    added routes scoring endpoint

--- backend/SafeBikeAPI/routes/admin.py
+++ backend/SafeBikeAPI/routes/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Route, Segment, Score, DangerCategory
 
-# Register your models here.
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner_id", "id")
+    search_fields = ("name",)
+    filter_horizontal = ("segments",)
+
+
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ("src_x", "src_y", "dest_x", "dest_y", "id")
+    search_fields = ("src_x", "src_y", "dest_x", "dest_y")
+
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = (
+        "owner_id",
+        "segment_id",
+        "danger_category_id",
+        "score",
+        "id",
+    )
+    search_fields = ("owner_id", "segment_id", "danger_category_id", "score")
+
+
+@admin.register(DangerCategory)
+class DangerCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_score", "max_score", "is_negative", "id")
+    search_fields = ("name", "min_score", "max_score", "is_negative")
--- backend/SafeBikeAPI/routes/views.py
+++ backend/SafeBikeAPI/routes/views.py
@@ -1,5 +1,6 @@
 from rest_framework import views
 from .models import Route, Segment, Score, DangerCategory
+from users.models import User
 from .serializers import (
     RouteSerializer,
     SegmentSerializer,
@@ -21,20 +22,28 @@ class ListRoutesView(views.APIView):
 class RateView(views.APIView):
     # POST /route/$route-id/rate
     # request body:
-    #  {“route_id”: “$route_id”, segment_ratings: {“segment_idx”: number, “danger_category_idx”: number, “value”: number}[]}
+    #  {“route_id”: “$route_id”, segment_ratings:
+    # {“segment_idx”: number, “danger_category_idx”: number, “value”: number}[]}
     def post(self, request, route_id):
         user_id = request.user.id
+        if not user_id:
+            user_id = 1
+        user = User.objects.get(id=user_id)
         route = Route.objects.get(id=route_id)
         segments_ratings = request.data["segment_ratings"]
         for segment_rating in segments_ratings:
             segment_id = segment_rating["segment_idx"]
+            segment = Segment.objects.get(id=segment_id)
             danger_category_id = segment_rating["danger_category_idx"]
+            danger_category = DangerCategory.objects.get(id=danger_category_id)
             score = segment_rating["value"]
             score = Score(
-                owner_id=user_id,
-                segment_id=segment_id,
-                danger_category_id=danger_category_id,
+                owner_id=user,
+                segment_id=segment,
+                danger_category_id=danger_category,
                 score=score,
             )
             route.segments.add(segment_id)
             score.save()
+
+        return Response({"message": "Rating saved successfully"})
