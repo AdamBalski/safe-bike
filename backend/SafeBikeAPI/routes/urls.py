1308fc7648de7940ef24078d793c42adf39e7640
Kacper Wojtowicz <31704690+kapiw04@users.noreply.github.com>
2024-09-28 21:16:03 +0200

    added routes scoring endpoint (#12)

--- backend/SafeBikeAPI/routes/urls.py
+++ backend/SafeBikeAPI/routes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListRoutesView
+from .views import ListRoutesView, RateView
 
 urlpatterns = [
     path("routes/", ListRoutesView.as_view(), name="routes"),
+    path("route/<int:route_id>/score/", RateView.as_view(), name="score"),
 ]
