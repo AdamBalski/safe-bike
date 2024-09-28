d4424b512ead705e9eccc41cd86583944235cd4a
kapiw04 <wojtowicz.kacper@gmail.com>
2024-09-28 19:46:51 +0200

    settings.py update; basic apiviews

--- backend/SafeBikeAPI/SafeBikeAPI/settings.py
+++ backend/SafeBikeAPI/SafeBikeAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
