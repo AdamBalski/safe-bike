e6e994575361ae995a18510d96ddfd6c517d00bb
kapiw04 <wojtowicz.kacper@gmail.com>
2024-09-28 13:46:08 +0200

    route get and post

---
+++ backend/routers/routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+dummy_route = [
+    {
+        "id": 1,
+        "owner_id": 1,
+        "name": "Route 1",
+        "points": [
+            {"x": 0, "y": 0},
+            {"x": 1, "y": 1},
+            {"x": 2, "y": 2},
+        ],
+        "segments": [
+            {"id": 1, "name": "Segment 1"},
+            {"id": 2, "name": "Segment 2"},
+        ],
+    }
+]
+
+
+@router.get("/routes")
+async def get_routes():
+    return dummy_route
+
+
+@router.post("/routes")
+async def post_route(route: str):
+    return {"route": route}
